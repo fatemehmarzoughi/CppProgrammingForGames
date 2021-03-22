86360f0448ef72a41b10df3ba4b7414f63326d58
Fatemeh <fatemeh.marzoughi18@gmail.com>
2021-03-22 20:22:19 +0330

    factorial

---
+++ Chapter3/factorial/include/Foo.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * This is a foo class to demonstrate features of the boiler plate.
+ */
+class Foo {
+	public:
+		// result = 1;
+  /**
+   * Default constructor for foo (does nothing).
+   */
+	 
+  Foo();
+  /**
+   * Returns a bool.
+   * @return Always True.
+   */
+
+  int factorial(int);
+};
