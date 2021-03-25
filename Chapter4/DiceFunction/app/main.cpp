c24287c5f67b20bcaf1d3323cc84c7bed42b8804
Fatemeh <fatemeh.marzoughi18@gmail.com>
2021-03-25 21:20:01 +0330

    DiceFunction

--- Chapter4/DiceFunction/app/main.cpp
+++ Chapter4/DiceFunction/app/main.cpp
@@ -1,12 +1,53 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
 
-#include "Foo.h"
+void Dice(int & die1, int & die2)
+{
+  //gives a random number between 1 and 6
+  die1 = 1 + std::rand() % (6+1) - 1;
+  die2 = 1 + std::rand() % (6+1) - 1;
+}
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
+  srand( time(0) );
+
+  //x and y are the dies
+  int x = 0;
+  int y = 0;
+
+
+  // the user's score
+  int score = 1000;
+  bool input = 0;
+
+  while(!input)
+  {
+    if(score <= 0)
+    {
+      std::cout<<"Loser!!"<<std::endl;
+      break;
+    }
+    std::cout<<"Enter your choice : 0)play 1)quit"<<std::endl;
+    std::cin>>input;
+    if(input == 1)
+      break;
+    Dice(x , y);
+    std::cout<<x<<"  ,  "<<y<<std::endl;
+
+    if(x == y && x == 6)
+    {
+      score *= 2 ;
+    }
+    else
+    {
+      score -= 100;
+    }
+
+    std::cout<<"Score = "<<score<<std::endl;
+  }
 
   return 0;
 }
